84e6f2dd9538e7ffcc0f71e9b1e245b7f695987e
RAHAT11790 <rahatsarker224@gmail.com>
2025-11-27 11:52:43 +0600

    Upload config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import re, os
+
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = 25976192
+
+API_HASH = "8ba23141980539b4896e5adbc4ffd2e2"
+
+BOT_TOKEN = "8384469114:AAHtHohk0PYCCjngic6pqWymEQLKkVqCq98"
+
+FORCE_SUB = "CARTOONFUNNY03"
+
+DB_NAME = "ranem_bot" 
+
+DB_URL = "mongodb+srv://RAHAT1132:RAHAT11a@rahat.txn4lkk.mongodb.net/?appName=Rahat"
+ 
+FLOOD = 10
+
+START_PIC = "https://ibb.co.com/qLbYJwK9"
+
+ADMIN = [6621572366]
+
+PORT = 8080
